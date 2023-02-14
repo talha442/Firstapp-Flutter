2f18ef665b91eca6adc7a8e3c60eabe963a15a21
talha faisal <57016608+talha442@users.noreply.github.com>
2023-02-14 15:10:07 +0500

    Load and Decoded JSON

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,31 @@
 // ignore_for_file: file_names, avoid_unnecessary_containers, prefer_const_constructors, use_key_in_widget_constructors
 
 import 'package:firstapp/models/catalog.dart';
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '../widgets/drawer.dart';
 import '../widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+  }
+
   @override
   Widget build(BuildContext context) {
     final dummpyList = List.generate(5, (index) => CatalogModel.items[0]);
